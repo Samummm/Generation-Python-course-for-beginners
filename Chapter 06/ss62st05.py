dd0379dc8f928c9bd0ae9d4305aeb749cb88c5d7
Mad Max <samumm@mail.ru>
2022-12-28 17:39:25 +0300

    Chapter 6 Subsection 6.2 Step All

---
+++ Chapter 06/ss62st05.py
@@ -0,0 +1,8 @@
+# Напишите программу, которая выводит текст:
+#
+# "Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+#
+# Примечание. Используйте конкатенацию строк.
+
+# put your python code here
+print('"Python is a great language!", said Fred. "I' + " don't ever remember having this much fun" + ' before."')

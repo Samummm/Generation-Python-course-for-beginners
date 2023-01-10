d77ae439d5548a158c1d1d08fcb59bb30f7e6495
Mad Max <samumm@mail.ru>
2023-01-10 15:19:13 +0300

    Chapter 9 Subsection 9.1 Step All

---
+++ Chapter 09/subSection 9.1/ss91st05.py
@@ -0,0 +1,6 @@
+# Дополните приведенный код, используя индексатор, так чтобы он вывел символ запятой.
+
+# put your python code here
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+
+print(s[7])
